b4ee4ed94924e5cd2b30ebc87128b172ee942885
NyX1337 <bdnov123@hotmail.com>
2021-09-29 11:23:43 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import { SafeAreaView, StyleSheet, View, Text } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      <View style={styles.header}>
+        <Text style={styles.todoText}>TODO APP</Text>
+        <Icon name='delete' size={25} color='red' />
+      </View>
+      <View style={styles.footer}>
+        <View style={styles.container}></View>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  header: {
+    padding: 20,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
-});
+  todoText: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: 'blue',
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    color: 'white',
+    width: '100%',
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+  },
+  container: {},
+})
